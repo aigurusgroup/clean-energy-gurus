d34bcf2af380ed9150b9b69f408b99f453ed50bf
aigurusgroup <geouffrey@aigurusgroup.com>
2026-06-01 12:33:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')}>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
